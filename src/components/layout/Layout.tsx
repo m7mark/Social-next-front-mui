e365bf74f7b20fea24669f908ee936a2f797e129
Mark <m7mark@yandex.ru>
2023-03-30 20:07:33 +0300

    add logo

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,7 @@
 import clsx from 'clsx'
+import Image from 'next/image'
 import { PropsWithChildren } from 'react'
+import logo from '../../shared/img/logo.svg'
 import styles from './Layout.module.scss'
 import { MainHeader } from './MainHeader/MainHeader'
 import { Sidebar } from './Sidebar/Sidebar'
@@ -19,7 +21,9 @@ export const Layout = ({ children, displayName }: LayoutProps) => {
       {displayName !== 'AuthPage' && (
         <MainHeader className={styles.headerText} />
       )}
-      <div className={styles.headerLogo}>Logo</div>
+      <div className={styles.headerLogo}>
+        <Image src={logo} alt="logo image" height={30} />
+      </div>
       <div className={styles.headerBg}></div>
       {displayName !== 'AuthPage' && <Sidebar className={styles.sidebar} />}
       <main>{children}</main>
